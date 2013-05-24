1dc9e856635e5b9f7b1bea636c4dcfc5800ff3f0
Paolo Fragomeni <paolo@async.ly>
2013-05-24 13:20:16 -0400

    [tests] a simple benchmark to kick things off

---
+++ test/bench.js
@@ -0,0 +1,28 @@
+
+var Benchmark = require('benchmark')
+var suite = new Benchmark.Suite
+
+var winston = require('winston')
+winston.add(winston.transports.File, { filename: 'logs.log' });
+
+var loqui = require('../index').createClient()
+var log = []
+
+// add tests
+suite.add('Winston', function() {
+  winston.log('info', 'Hello distributed log files!')
+})
+.add('Loqui', function() {
+  loqui.log('info', 'Hello distributed log files!')
+})
+
+// add listeners
+.on('cycle', function(event) {
+  log.push(String(event.target))
+})
+.on('complete', function() {
+  console.log(log)
+  console.log('Fastest is ' + this.filter('fastest').pluck('name'))
+})
+// run async
+.run({ 'async': true })
